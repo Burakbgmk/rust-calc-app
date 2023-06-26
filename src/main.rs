5e1ec06cd477ea37ecc3b5d629397c5e4fec664d
Burak Boğmak <burakbogmak@gmail.com>
2023-06-26 20:25:29 +0300

    refactored calculation logic

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,18 @@
-use std::collections::HashMap;
 
 fn main() {
-    // println!("Hello, world!");
-    // let add = Operation::Addition { x: 3, y: 5 };
-    // let sub = Operation::Substruction { op_str: "7-5".to_string() };
-    // let mult = Operation::Multiplication { op_str: "2*5".to_string() };
-    // let div = Operation::Division { op_str: "9/3".to_string() };
-    // println!("{}",add.operate());
-    // println!("{}",sub.operate());
-    // println!("{}",mult.operate());
-    // println!("{}",div.operate());
-    // let mapp = map_operators("2+5*8/3".to_string());
-    // for ele in mapp {
-    //     println!("Key is {}, Value is {}",ele.0,ele.1);
-    // }
+
+    let result = parse_calculations("33/2+7".to_string());
+    println!("RESULT IS : {}",result);
+
 
 }
 
-fn map_operators(calculation_string: String) -> HashMap<i32, String> {
-    let mut operator_map: HashMap<i32, String> = HashMap::new();
+fn map_operators(calculation_string: &str) -> Vec<i32> {
+    let mut operator_map:Vec<i32> = Vec::new();
     let mut index = 0;
     for c in calculation_string.chars() {
         if c.eq(&'*') || c.eq(&'/') || c.eq(&'+') || c.eq(&'-') {
-            let operator = c.to_string();
-            operator_map.insert(index,operator);
+            operator_map.push(index);
         }
         index += 1;
     }
@@ -32,62 +21,103 @@ fn map_operators(calculation_string: String) -> HashMap<i32, String> {
 
 
 
-fn parse_calculation_string(calculation_string: String) {
-    
-
-}
-
-enum Symbol {
-    Add,
-    Substract,
-    Multiply,
-    Divide
-}
-
-impl Symbol {
-    fn get(&self) -> String {
-        match self {
-            Symbol::Add => "+".to_string(),
-            Symbol::Substract => "-".to_string(),
-            Symbol::Multiply => "*".to_string(),
-            Symbol::Divide => "/".to_string()
-        }
+fn parse_calculations(calculation_string: String) -> String {
+    let opts = vec!['*','/','+','-'];
+    let operator_map: Vec<i32> = map_operators(&calculation_string);
+    println!("operators mapped with length: {}",operator_map.len());
+    if operator_map.len().eq(&0) || calculation_string[0..1].eq("-") {
+        println!("DONE");
+        return calculation_string;
     }
+
+    let length = calculation_string.len();
+    println!("calculation string is {} and length is {}",calculation_string,length);
+    let mut i = 0;
+    while i < length {
+        let index:i32 = i as i32;
+        let c = calculation_string.chars().nth(i).unwrap();
+        println!("Iteration {} is started and character is {}",index,c);
+        if opts.contains(&c) {
+            let current_index_at = operator_map.iter().position(|&r| r.eq(&index)).unwrap();
+            let mut before_index = 0;
+            if current_index_at != 0 {
+                before_index = operator_map[current_index_at-1] as usize;
+                before_index += 1;
+            }
+            let mut after_index = length;
+            if operator_map.len() > 1 {
+                after_index = operator_map[current_index_at+1] as usize;
+
+            }
+            let opt_str = calculation_string[before_index..after_index].to_string();
+            let result;
+            println!("Before index: {} - After index: {}",before_index,after_index);
+            println!("optstr is {}",opt_str);
+            if c.eq(&'*') {
+                result = Operation::Multiplication { op_str: opt_str }.operate().to_string();
+            } else if c.eq(&'/') {
+                result = Operation::Division { op_str: opt_str }.operate().to_string();
+            } else if c.eq(&'+') {
+                result = Operation::Addition { op_str: opt_str }.operate().to_string();
+            } else if c.eq(&'-') {
+                result = Operation::Substruction { op_str: opt_str }.operate().to_string();
+            } else {
+                continue;
+            }
+            println!("operation is done");
+            let new_calc_str = calculation_string[0..before_index].to_string() + &result + &calculation_string[after_index..calculation_string.len()].to_string();
+            println!("new string is {}",new_calc_str);
+            return parse_calculations(new_calc_str);
+            
+        }
+        i += 1;
+    }
+    return "".to_string();
+
 }
+
+
 
 
 enum Operation {
-    Addition{x: i32, y: i32},
-    Substruction{x: i32, y: i32},
-    Multiplication{x: i32, y: i32},
-    Division{x: i32, y: i32}
+    Addition{op_str: String},
+    Substruction{op_str: String},
+    Multiplication{op_str: String},
+    Division{op_str: String}
 }
 
 impl Operation {
     fn operate(&self) -> String {
+        
         match self {
-            Operation::Addition {x,y} => {
-                // let x = op_str.chars().nth(0).unwrap().to_digit(10).unwrap();
-                // let y = op_str.chars().nth(2).unwrap().to_digit(10).unwrap();
-                let z = x + y;
+            Operation::Addition {op_str} => {
+                let index_of_operator = op_str.chars().into_iter().position(|r| r.eq(&'+')).unwrap();
+                let x = op_str[0..index_of_operator].parse::<f64>().unwrap();
+                let y = op_str[index_of_operator+1..op_str.len()].parse::<f64>().unwrap();
+                let z = x as f64 + y as f64;
                 return z.to_string()
             },
-            Operation::Substruction {x,y} => {
-                // let x = op_str.chars().nth(0).unwrap().to_digit(10).unwrap();
-                // let y = op_str.chars().nth(2).unwrap().to_digit(10).unwrap();
-                let z = x - y;
+            Operation::Substruction {op_str} => {
+                let index_of_operator = op_str.chars().into_iter().position(|r| r.eq(&'-')).unwrap();
+                let x = op_str[0..index_of_operator].parse::<f64>().unwrap();
+                let y = op_str[index_of_operator+1..op_str.len()].parse::<f64>().unwrap();
+                let z = x as f64 - y as f64;
                 return z.to_string()
             },
-            Operation::Multiplication {x,y} => {
-                // let x = op_str.chars().nth(0).unwrap().to_digit(10).unwrap();
-                // let y = op_str.chars().nth(2).unwrap().to_digit(10).unwrap();
-                let z = x * y;
+            Operation::Multiplication {op_str} => {
+                let index_of_operator = op_str.chars().into_iter().position(|r| r.eq(&'*')).unwrap();
+                let x = op_str[0..index_of_operator].parse::<f64>().unwrap();
+                let y = op_str[index_of_operator+1..op_str.len()].parse::<f64>().unwrap();
+                let z = x as f64 * y as f64;
                 return z.to_string()
             },
-            Operation::Division {x,y} => {
-                // let x = op_str.chars().nth(0).unwrap().to_digit(10).unwrap();
-                // let y = op_str.chars().nth(2).unwrap().to_digit(10).unwrap();
-                let z = x / y;
+            Operation::Division {op_str} => {
+                let index_of_operator = op_str.chars().into_iter().position(|r| r.eq(&'/')).unwrap();
+                println!("sçdöfm");
+                let x = op_str[0..index_of_operator].parse::<f64>().unwrap();
+                let y = op_str[index_of_operator+1..op_str.len()].parse::<f64>().unwrap();
+                println!("x: {} and y : {}",x,y);
+                let z = x as f64 / y as f64;
                 return z.to_string()
             }
         }
